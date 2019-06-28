da97ef3e4b91ce2849cf45a3f289016d3e3fe8e9
Repulser <thetechproject2014@gmail.com>
2019-06-28 15:29:46 +0300

    Change design to use custom API

--- ml.py
+++ ml.py
@@ -1,37 +1,23 @@
 from typing import Tuple, Optional
 
-from google.cloud import automl_v1beta1
-from google.oauth2 import service_account
+import aiohttp
 
 import utils
 
+session = aiohttp.ClientSession()
 config = utils.load_config()
-credentials = service_account.Credentials.from_service_account_file('automl_credentials.json')  # GCP Credentials
-prediction_client = automl_v1beta1.PredictionServiceClient(credentials=credentials)  # Prediction client
 
 
-def get_prediction(content: str, project_id: str, model_id: str):
+async def predict(content: str) -> Optional[Tuple[str, float]]:
     """
-    Requests prediction from google AutoML.
-    :param content: Content of message
-    :param project_id: Project
-    :param model_id:
-    :return: PredictionResponse object from google
-    """
-    name = 'projects/{}/locations/us-central1/models/{}'.format(project_id, model_id)
-    payload = {'text_snippet': {'content': content, 'mime_type': 'text/plain'}}
-    params = {}
-    request = prediction_client.predict(name, payload, params)
-    return request  # waits till request is returned
-
-
-def predict(content: str) -> Optional[Tuple[str, float]]:
-    """
-    Prediction wrapper.
+    Prediction API Wrapper.
     :param content: Content of message
     :return:
     """
-    prediction = get_prediction(content, config['project_id'], config['model_id'])
-    if not prediction:
-        return None
-    return prediction.payload[0].display_name, prediction.payload[0].classification.score
+    base_url = config['api_base']
+    async with session.get(f'{base_url}/predict?content={content}') as response:
+        if response.status != 200:
+            print(await response.text())
+            return None
+        prediction = await response.json()
+    return prediction["classification"], prediction["score"]
